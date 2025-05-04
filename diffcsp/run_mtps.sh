1f7764fedd11e413176a174b3bafb70d11233e6b
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-05-04 22:57:10 +0530

    push from local

--- diffcsp/run_mtps.sh
+++ diffcsp/run_mtps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=CrysLDM        # Job name
-#SBATCH --output=crysldm_mp.out
-#SBATCH --error=crysldm_mp.err
+#SBATCH --job-name=DiffCSP        # Job name
+#SBATCH --output=diffcsp_mpts.out
+#SBATCH --error=diffcsp_mpts.err
 #SBATCH --time=12:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
@@ -19,4 +19,6 @@ source /home/rs1/21CS92R01/anaconda3/bin/activate CrysLDM
 
 #python3 -W ignore run.py data=mpts_52 model=diffusion_w_type expname=mpts_52
 
-python3 -W ignore generation.py --model_path /home/rs1/21CS92R01/workplace/DiffCSP_forked/singlerun/2025-05-04/mpts_52 --dataset mpts_52
+#python3 -W ignore generation.py --model_path /home/rs1/21CS92R01/workplace/DiffCSP_forked/singlerun/2025-05-04/mpts_52 --dataset mpts_52
+
+python3 -W ignore compute_metrics.py --root_path /home/rs1/21CS92R01/workplace/DiffCSP_forked/singlerun/2025-05-04/mpts_52/ --tasks gen --gt_file data/mpts_52/test.csv
